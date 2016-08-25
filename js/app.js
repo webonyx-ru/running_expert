235d829945574f2b4eefd5a172aa64cc30f87ee2
shari <sharipkanov@gmail.com>
2016-08-25 16:37:41 +0600

    index page

--- js/app.js
+++ js/app.js
@@ -2,9 +2,21 @@ $(document).ready(function () {
     $('.menu-btn, .main-menu-btn').click(function (event) {
         event.preventDefault();
 
-        var currentNav = $(this).attr('href');
+        var currentNav = $(this).attr('href'),
+            headerSection = $('.header-section');
 
         $('.navigation').removeClass('active');
+        headerSection.addClass('header-active');
+
+        if(!headerSection.hasClass('header-big')) {
+            var elementHeights = $('.navigation-info').map(function() {
+                return $(this).height();
+            }).get();
+
+            var maxHeight = Math.max.apply(null, elementHeights);
+
+            $('.header-active').height(maxHeight);
+        }
 
         $(currentNav).addClass('active');
 
@@ -15,6 +27,7 @@ $(document).ready(function () {
         event.preventDefault();
 
         $('.navigation').removeClass('active');
+        $('.header-section').removeClass('header-active');
 
         return false;
     });
